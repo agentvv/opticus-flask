287927af1eff76e9974df1919ebd0e13e727e0e6
Cameron Dufault <44153501+dufaultc@users.noreply.github.com>
2021-03-07 11:50:14 -0500

    Added paper size model

--- ML/interface.py
+++ ML/interface.py
@@ -54,9 +54,46 @@ def getFaceMeasurements(f):
     coords = get_preds(output)
     coords = coords.squeeze()
     coords = coords.numpy()
+    temple_points = [coords[0], coords[16]]
+    eye_points = [coords[39], coords[42]]
 
-    faceMeasurementsCoords = [int(( (abs(coords[16][0] - coords[ 0][0]))**2 + (abs(coords[16][1] - coords[ 0][1]))**2 )**(1/2)),
-                              int(( (abs(coords[42][0] - coords[39][0]))**2 + (abs(coords[42][1] - coords[39][1]))**2 )**(1/2))]
+
+    device = torch.device('cpu')
+    fcn_load = models.segmentation.fcn_resnet101(pretrained=False)
+    fcn_load.classifier = FCNHead(2048,2)
+    fcn_load.aux_classifier = FCNHead(1024,2)
+    #state_dict = torch.load("./paper_model", map_location=device)
+    fcn_load.load_state_dict(torch.load("./paper_model", map_location=device))
+
+    base = './paper_dataset/20210307_003056 (2).jpg'
+    fcn_load.aux_classifier = None
+    trf_x = T.Compose([T.Resize([256,256]),
+                 T.ToTensor(), 
+                 T.Normalize(mean = [0.485, 0.456, 0.406], 
+                             std = [0.229, 0.224, 0.225])])
+    out = trf_x(Image.open(os.path.join(base)).convert('RGB'))
+    out= out.unsqueeze(0)
+    out = fcn_load(out.float())['out']
+    om = torch.argmax(out.squeeze(), dim=0).detach().cpu().numpy()
+    om_list = list(om)
+    for i in range(len(om_list)):
+        if 0 in list(om_list[i]):
+            top = i
+            print(top)
+            break
+    for j in range(len(om_list)):
+        if 0 in list(om_list[len(om_list)-1 - j ]):
+            print(len(om_list)-1 - j)
+            bottom = len(om_list)-1 - j 
+            break
+    distance  = bottom-top
+    dist_eye = np.linalg.norm(eye_points[0]-eye_points[1])*4
+    dist_temple =  np.linalg.norm(temple_points[0]-temple_points[1])*4
+    out_eye = 11.5 * ((dist_eye*0.5)/distance)
+    out_temple = 11.5 * (dist_temple/distance)
+    faceMeasurementsCoords = [int(out_temple),int(out_eye)]
+    #faceMeasurementsCoords = [int(( (abs(coords[16][0] - coords[ 0][0]))**2 + (abs(coords[16][1] - coords[ 0][1]))**2 )**(1/2)),
+    #                          int(( (abs(coords[42][0] - coords[39][0]))**2 + (abs(coords[42][1] - coords[39][1]))**2 )**(1/2))]
 
     # ~ res = cv2.resize(img[2,:,:], dsize=(64, 64), interpolation=cv2.INTER_CUBIC )
     # ~ plt.imshow(res,cmap="cividis")
